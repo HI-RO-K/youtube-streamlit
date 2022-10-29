82b04ae92d3adfdef00e6f375f4fd856cbd7c448
HI-RO-K <I.hiroki.0509@outlook.jp>
2022-10-29 16:02:17 +0900

    1st commit

--- mainPython.py
+++ mainPython.py
@@ -1,15 +1,31 @@
-from tkinter import Image
 import streamlit as st
-import numpy as np
-import pandas as pd
-
+import time
 st.title('Streamlit 超入門')
 
-st.write('Display Image')
+st.write('Interactive Widgets')
 
-img = Image.open('sample.JPG')
+'Start!!'
 
-st.image(img, caption='book', use_column_width=True)
+latest_iteration =st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.01)
+
+option = st.sidebar.text_input('あなたの趣味を教えてください。')
+
+condition = st.sidebar.slider('あなたの調子は？', 0, 50, 100)
+'あなたの好きな趣味は、', option, 'です。'
+'コンディション：', condition
+
+
+# img = Image.open('sample.JPG')
+
+# st.image(img, caption='book', use_column_width=True)
+
+
 
 # df = pd.DataFrame(
 #     np.random.rand(100, 2)/[50, 50] + [35.69, 139.70],

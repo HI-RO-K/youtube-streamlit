ed4d8fc9d1631da03ea6528a8487ef8498e62bde
HI-RO-K <I.hiroki.0509@outlook.jp>
2022-10-29 14:53:52 +0900

    update streamlit

---
+++ mainPython.py
@@ -0,0 +1,22 @@
+from tkinter import Image
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title('Streamlit 超入門')
+
+st.write('Display Image')
+
+# img = Image.open('sample.JPG')
+
+# st.image(img, caption='book', use_column_width=True)
+
+# df = pd.DataFrame(
+#     np.random.rand(100, 2)/[50, 50] + [35.69, 139.70],
+#     columns=['lat', 'lon']
+# )
+
+# st.map(df)
+
+# st.table(df.style.highlight_max(axis=0))
+

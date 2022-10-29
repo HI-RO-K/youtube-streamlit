04bc738b91d4930429eedc9477aae0d5de84de95
HI-RO-K <I.hiroki.0509@outlook.jp>
2022-10-29 14:55:05 +0900

    update mainpython

--- mainPython.py
+++ mainPython.py
@@ -7,9 +7,9 @@ st.title('Streamlit 超入門')
 
 st.write('Display Image')
 
-# img = Image.open('sample.JPG')
+img = Image.open('sample.JPG')
 
-# st.image(img, caption='book', use_column_width=True)
+st.image(img, caption='book', use_column_width=True)
 
 # df = pd.DataFrame(
 #     np.random.rand(100, 2)/[50, 50] + [35.69, 139.70],
